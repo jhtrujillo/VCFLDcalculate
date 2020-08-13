a182901f547004a714c60a6520fcb522ae9a0b97
estuvar4 <estuvar4@estuvar4-Latitude-E5470>
2020-08-13 12:54:26 -0500

    Nuevos codigos

--- src/VCFldcalculator.java
+++ src/VCFldcalculator.java
@@ -32,16 +32,12 @@ public class VCFldcalculator {
 			if (snp.compareTo(SNP1) == 0) {
 				record1 = record;
 				recordsInMemory.add(record1);
-				// System.out.println(calls.get(0).getSequenceName() + " " +
-				// calls.get(0).getFirst());
 			}
 
 			if (snp.compareTo(SNP2) == 0) {
 				record2 = record;
 				recordsInMemory.add(record2);
 				break;
-				// System.out.println(calls.get(0).getSequenceName() + " " +
-				// calls.get(0).getFirst());
 			}
 
 		}
@@ -52,6 +48,292 @@ public class VCFldcalculator {
 	// --------------------------------------------
 	// Calculo de LD entre dos SNPs
 	// --------------------------------------------
+	public void CalculateLD2(String filename, String SNP1, String SNP2) throws IOException {
+		List<VCFRecord> recordsInMemory = loadVCF(filename, SNP1, SNP2);
+
+		VCFRecord record1 = recordsInMemory.get(0);
+		VCFRecord record2 = recordsInMemory.get(1);
+
+		List<CalledGenomicVariant> calls1 = record1.getCalls();
+		List<CalledGenomicVariant> calls2 = record2.getCalls();
+
+		int n = calls1.size();
+
+		double p1 = 0.0;
+		double p2 = 0.0;
+		double q1 = 0.0;
+		double q2 = 0.0;
+		double freHap = 0.0;
+		double Dprime=0;
+		int numInd = 0;
+		int numIndmissing=0;
+		double frecAB = 0;
+		double D=0;
+		double r2= 0;
+		int opc1=0;
+		int opc2=0;
+		int opc3=0;
+		int opc4=0;
+
+		// --------------------------------------------
+		// Frecuencia de los alelos menores y de haplotipo AB.
+		// --------------------------------------------
+		for (int i = 0; i < n; i++) {
+			CalledGenomicVariant call1 = calls1.get(i);
+			CalledGenomicVariant call2 = calls2.get(i);
+			
+			if(call1.isHomozygousReference() && call2.isHomozygousReference()) {
+				p1+=2;
+				q1+=2;
+				frecAB+=2;
+				opc1++;
+				numInd+=1;	
+			}
+			
+			
+			if(call1.isHomozygousReference() && call2.isHeterozygous()) {
+				p1+=2;
+				q1+=1;
+				frecAB+=1;
+				opc2++;
+				numInd+=1;
+			}
+			
+			
+			if(call1.isHeterozygous() && call2.isHomozygousReference()) {
+				p1+=1;
+				q1+=2;
+				frecAB+=1;
+				opc3++;
+				numInd+=1;
+			}
+	
+		}
+		
+		
+		p1 = p1/(numInd*2);
+		p2 = 1-p1;
+		q1 = q1/(numInd*2);
+		q2 = 1-q1;
+		
+		frecAB=frecAB/(numInd*2);
+		D = frecAB - p1*q1;
+		
+		
+		if (D>=0) {
+			Dprime = D / Math.max(p1*q2, p2*q1);
+		}else {
+			Dprime = D / Math.min(-p1*q1, -p2*q2);
+		}
+		
+		r2 = (D*D) / p1*q1*q1*q2 ;
+		
+		
+		//System.out.println("Total Ind : "+(numInd+numIndmissing) );
+		//System.out.println("Ind inclu : "+ numInd );
+		//System.out.println("Ind exclu : "+numIndmissing );
+		//System.out.println("Opcion  1: "+opc1);
+		//System.out.println("Opcion  2: "+opc2);
+		//System.out.println("Opcion  3: "+opc3);
+		System.out.println("Opcion p1 : " + p1 );
+		System.out.println("Opcion q1 : " + q1 );
+		System.out.println("Fre p1*q1 : " + p1*q1 );
+		System.out.println("frecAB    : " + frecAB );
+		System.out.println("DAB       : " + D );
+		System.out.println("Dprime    : " + Dprime );
+		System.out.println("R2        : " + r2);
+		
+	}
+	
+	
+	
+	// --------------------------------------------
+		// Calculo de LD entre dos SNPs
+		// --------------------------------------------
+		public void CalculateLD3(String filename, String SNP1, String SNP2) throws IOException {
+			List<VCFRecord> recordsInMemory = loadVCF(filename, SNP1, SNP2);
+
+			VCFRecord record1 = recordsInMemory.get(0);
+			VCFRecord record2 = recordsInMemory.get(1);
+
+			List<CalledGenomicVariant> calls1 = record1.getCalls();
+			List<CalledGenomicVariant> calls2 = record2.getCalls();
+
+			int n = calls1.size();
+
+			double p1 = 0.0;
+			double p2 = 0.0;
+			double q1 = 0.0;
+			double q2 = 0.0;
+			double freHap = 0.0;
+			double Dprime=0;
+			int numInd = 0;
+			int numIndmissing=0;
+			double frecAB = 0;
+			double D=0;
+			double r2= 0;
+			int opc1=0;
+			int opc2=0;
+			int opc3=0;
+			int opc4=0;
+
+			// --------------------------------------------
+			// Frecuencia de los alelos menores y de haplotipo AB.
+			// --------------------------------------------
+			for (int i = 0; i < n; i++) {
+				CalledGenomicVariant call1 = calls1.get(i);
+				CalledGenomicVariant call2 = calls2.get(i);
+				
+				
+				if(call1.isUndecided() || call1.isHeterozygous()) continue;
+				if(call2.isUndecided() || call2.isHeterozygous()) continue;
+						
+				numInd++;
+				
+				if(call1.isHomozygousReference()) {
+					p1++;
+					if(call2.isHomozygousReference()) frecAB++;
+				}
+				
+				if(call2.isHomozygousReference()) q1++;
+				
+			}
+			
+			p1 = p1/(numInd*2);
+			p2 = 1-p1;
+			q1 = q1/(numInd*2);
+			q2 = 1-q1;
+			
+			frecAB=frecAB/(numInd*2);
+			D = frecAB - p1*q1;
+			
+			
+			if (D>=0) {
+				Dprime = D / Math.max(p1*q2, p2*q1);
+			}else {
+				Dprime = D / Math.min(-p1*q1, -p2*q2);
+			}
+			
+			r2 = (D*D) / p1*q1*q1*q2 ;
+			
+			
+			//System.out.println("Total Ind : "+(numInd+numIndmissing) );
+			//System.out.println("Ind inclu : "+ numInd );
+			//System.out.println("Ind exclu : "+numIndmissing );
+			//System.out.println("Opcion  1: "+opc1);
+			//System.out.println("Opcion  2: "+opc2);
+			//System.out.println("Opcion  3: "+opc3);
+			System.out.println("Opcion p1 : " + p1 );
+			System.out.println("Opcion q1 : " + q1 );
+			System.out.println("Fre p1*q1 : " + p1*q1 );
+			System.out.println("frecAB    : " + frecAB );
+			System.out.println("DAB       : " + D );
+			System.out.println("Dprime    : " + Dprime );
+			System.out.println("R2        : " + r2);
+			
+		}
+	
+	
+		
+		// --------------------------------------------
+		// Calculo de LD entre dos SNPs
+		// --------------------------------------------
+		public void CalculateLD4(String filename, String SNP1, String SNP2) throws IOException {
+			List<VCFRecord> recordsInMemory = loadVCF(filename, SNP1, SNP2);
+
+			VCFRecord record1 = recordsInMemory.get(0);
+			VCFRecord record2 = recordsInMemory.get(1);
+
+			List<CalledGenomicVariant> calls1 = record1.getCalls();
+			List<CalledGenomicVariant> calls2 = record2.getCalls();
+
+			int n = calls1.size();
+
+			double p1 = 0.0;
+			double p2 = 0.0;
+			double q1 = 0.0;
+			double q2 = 0.0;
+			double frecuenciaExperadaAB=0;
+			double frecuenciaObservadaAB=0;
+			double D=0;
+			double numInd=0;
+			double Dprime=0;
+			double R2=0;
+			
+			// --------------------------------------------
+			// Frecuencia de los alelos menores y de haplotipo AB.
+			// --------------------------------------------
+			for (int i = 0; i < n; i++) {
+				CalledGenomicVariant call1 = calls1.get(i);
+				CalledGenomicVariant call2 = calls2.get(i);
+				
+				if(call1.isUndecided() || call2.isUndecided() ) continue;
+				if(call1.isHomozygous() && !call1.isHomozygousReference() && call2.isHomozygous() && !call2.isHomozygousReference() ) continue; 
+				
+				//calculo la frecuencia de los alelos de cada marcador.
+				if(call1.isHomozygousReference()) {
+					p1+=2;
+					p2+=0;
+				}
+				else if(call1.isHeterozygous()) {
+					p1+=1;
+					p2+=1;
+				}
+								
+				if(call2.isHomozygousReference()) {
+					q1+=2;
+					q2+=0;
+				}
+				else if(call2.isHeterozygous()) {
+					q1+=1;
+					q2+=1;
+				}
+				
+				//calculo la frecuencia del haplotipo Observadio AB
+				if( call1.isHomozygousReference() &&  call2.isHomozygousReference()  ) {
+					frecuenciaObservadaAB+=2;
+				}
+				
+				numInd++;
+			
+		
+			}
+			
+			p1/=numInd*2;
+			p2/=numInd*2;
+			q1/=numInd*2;
+			q2/=numInd*2;
+			
+			frecuenciaExperadaAB=p1*q1*(numInd*2);
+			frecuenciaObservadaAB/=(numInd*2);
+			
+			D = frecuenciaObservadaAB - p1*q1;
+			
+		
+			if (D>=0) {
+				Dprime = D / Math.min(p1*q2 , p2*q1);
+			}else {
+				Dprime = D / Math.max(-p1*q1 , -p2*q2);
+			}
+			
+			R2= (D*D)/p1*p2*q1*q2;
+			
+			
+			System.out.println("Opcion p1 : " + p1 );
+			System.out.println("Opcion p2 : " + p2 );
+			System.out.println("Opcion q1 : " + q1 );
+			System.out.println("Opcion q2 : " + q2 );
+			System.out.println("Expected  : " +  frecuenciaExperadaAB);
+			System.out.println("Observed  : " +  frecuenciaObservadaAB*(numInd*2));
+			System.out.println("D         : " +  D);
+			System.out.println("Dprime    : " +  Dprime);
+			System.out.println("R2        : " +  R2);
+			
+		
+			
+		}
+		
+		
 	public void CalculateLD(String filename, String SNP1, String SNP2) throws IOException {
 		List<VCFRecord> recordsInMemory = loadVCF(filename, SNP1, SNP2);
 
@@ -78,7 +360,7 @@ public class VCFldcalculator {
 
 			if (call1.isUndecided() || call2.isUndecided())
 				continue;
-			if (call1.isHeterozygous() & call2.isHeterozygous())
+			if (call1.isHeterozygous() && call2.isHeterozygous())
 				continue;
 
 			numInd++;
@@ -134,33 +416,32 @@ public class VCFldcalculator {
 		else
 			r2 /= (p1 * q1 * (1 - p1) * (1 - q1));
 
-		System.out.print("NumInd\t");
-		// System.out.print("p1\t");
-		// System.out.print("q1\t");
-		// System.out.print("Frec\t");
-		System.out.print("D\t");
-		System.out.print("Dprime\t");
-		System.out.print("R2\t");
-		System.out.println("r2\t");
-
-		System.out.print(numInd + "\t");
-		// System.out.print(p1+"\t");
-		// System.out.print(q1+"\t");
-		// System.out.print(p1 * q1+"\t");
-		System.out.print(D + "\t");
-		System.out.print(dPrime + "\t");
-		System.out.print(R2 + "\t");
-		System.out.println(r2 + "\t");
+		
+		System.out.println("Opcion p1 : " + p1 );
+		System.out.println("Opcion q1 : " + q1 );
+		System.out.println("Fre p1*q1 : " + p1*q1 );
+		System.out.println("frecAB    : " + frecHap );
+		System.out.println("DAB       : " + D );
+		System.out.println("Dprime    : " + dPrime );
+		System.out.println("R2        : " + r2);
 
 	}
 
 	public static void main(String[] args) throws IOException {
-		//VCFldcalculator ld = new VCFldcalculator();
-		//ld.CalculateLD("/home/estuvar4/Desktop/mergevcf.95ids.b_fourthfiltered.vcf", "scaffold_12997_216675",
-		//	"scaffold_12997_217253");
-
+		VCFldcalculator ld = new VCFldcalculator();
+		
+		//ld.CalculateLD("/home/estuvar4/git/VCFLDcalculate/vcf/mergevcf.95ids.b_fourthfiltered.vcf", "scaffold_10012_793871","scaffold_10012_700758");
+		//System.out.println("--------------------------------------");
+		//ld.CalculateLD2("/home/estuvar4/git/VCFLDcalculate/vcf/mergevcf.95ids.b_fourthfiltered.vcf", "scaffold_10012_793871","scaffold_10012_700758");
+		//System.out.println("--------------------------------------");
+		ld.CalculateLD3("/home/estuvar4/git/VCFLDcalculate/vcf/mergevcf.95ids.b_fourthfiltered.vcf", "scaffold_10012_793871","scaffold_10012_700758");
+		System.out.println("--------------------------------------");
+		ld.CalculateLD4("/home/estuvar4/git/VCFLDcalculate/vcf/mergevcf.95ids.b_fourthfiltered.vcf", "scaffold_10012_793871","scaffold_10012_700758");
+		
+		
 		VCFldcalculator r2ld = new VCFldcalculator();
-
+		
+		/*
 		try {
 			String opcion = args[0];
 
@@ -180,7 +461,7 @@ public class VCFldcalculator {
 		} catch (Exception e) {
 			System.out.println("Try java -jar fingerprint.jar -help");
 			System.out.println("Try java -jar ld.jar 1 vcf SNP1 SNP2");
-		}
+		}*/
 
 	}
 }
